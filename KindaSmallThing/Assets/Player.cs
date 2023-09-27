26ac14732d9d2787048d71ff8deac700ec8ba883
Luiz Henrique da Silva Santos <20221170150040@ifrn.local>
2023-09-27 10:21:58 -0300

    Ball walks I guess

--- KindaSmallThing/Assets/Player.cs
+++ KindaSmallThing/Assets/Player.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int speed = 10;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        Debug.Log("START");
+        TryGetComponent(out rb);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(h,0,v);
+        rb.AddForce(direction * speed * Time.deltaTime,ForceMode.Impulse);
+
+        if(transform.position.y <= -1){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
